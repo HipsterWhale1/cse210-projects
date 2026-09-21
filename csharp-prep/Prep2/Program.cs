549d703f8194aac46bcc7cf15d2bd4db9d3c0e86
HipsterWhale1 <matthewbvan@gmail.com>
2026-09-20 18:37:05 -0600

    modified prep1 and prep2 assignments

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,70 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        int percentage;
+        string letter = "letter was not assigned";
+        string modifier = "modifier was not assigned";
+        bool passed = false;
+
+        //get user input
+        Console.Write("Hello user! Whats your grade percentage right now? ");
+        percentage = int.Parse(Console.ReadLine());
+        
+        //determine letter grade
+        if (percentage % 10 >= 7)
+        {
+            modifier = "+" ;
+        }
+        else if (percentage % 10 <= 3)
+        {
+            modifier = "-";
+        }
+        else
+        {
+            modifier = "";
+        }
+
+        //determine modifier (+ or -)
+        if (percentage >= 90)
+        {
+            letter = "A";
+            passed = true;
+            if (percentage > 93)
+            {
+                modifier = "";
+            }
+        }
+        else if (percentage >= 80)
+        {
+            letter = "B";
+            passed = true;
+        }
+        else if (percentage >= 70)
+        {
+            letter = "C";
+            passed = true;
+        }
+        else if (percentage >= 60)
+        {
+            letter = "D";
+        }
+        else
+        {
+            letter = "F";
+            modifier = "";
+        }
+
+        // print grade
+        Console.WriteLine($"Your grade is {letter}{modifier}");
+
+        // print pass/fail
+        if (passed == true)
+        {
+            Console.WriteLine("Congratulations! You passed the class!");
+        }
+        else
+        {
+            Console.WriteLine("You put in great effort, but you did not pass the class. With help, we believe you can!");
+        }
     }
 }
